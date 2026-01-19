119f234821511927ed3fcb2410074c73b2368083
kayod <kay.odenthal@gmx.de>
2026-01-19 15:37:55 +0100

    add todo http service

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {Header} from './header/header';
+import {Todo, TodoService} from './services/todo.service';
 
 
 export interface User {
@@ -13,9 +14,19 @@ export interface User {
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
   protected readonly user: User = {
     firstName: 'Max',
     lastName: 'Mustermann',
+  }
+  private readonly todoService = inject(TodoService);
+  protected todos: Todo[] = [];
+
+  ngOnInit(): void {
+    this.todoService.getTodos().subscribe({
+      next: (todos) => {
+        this.todos = todos
+      }
+    })
   }
 }
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,8 +1,28 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {map, Observable} from 'rxjs';
+
+
+export interface Todo {
+  id: 1,
+  todo: "Do something nice for someone I care about",
+  completed: true,
+  userId: 26
+}
+
+export interface PaginatedResult {
+  todos: Todo[];
+}
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
-  
+  private readonly baseUrl = 'https://dummyjson.com/todos';
+  private readonly http = inject(HttpClient);
+
+  getTodos(): Observable<Todo[]> {
+    return this.http.get(this.baseUrl).pipe(map((result) => (result as PaginatedResult).todos));
+  }
 }
